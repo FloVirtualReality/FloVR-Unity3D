1574b99fdfac6fda697b1735afd49f1254f8ccca
Agócs Ádám <aagocs@me.com>
2016-12-20 18:28:22 +0100

    Preroll scene

---
+++ FloVR/Scripts/Preroller.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+namespace FloVR
+{
+	public class Preroller : MonoBehaviour
+	{
+		void Awake()
+		{
+			AsyncOperation sceneLoading = SceneManager.LoadSceneAsync(1, LoadSceneMode.Single);
+			sceneLoading.allowSceneActivation = false;
+			Ad newAd = new Ad(AdType.Video360, false, (AdState adState, Ad adObject) =>
+			{
+				if (adState == AdState.Ready) {
+					adObject.Show();
+				}
+				else if (adState == AdState.Shown) {
+					sceneLoading.allowSceneActivation = true;
+				}
+			});
+		}
+	}
+}
